d42f0649465047cefd5309f82b594aa43101848a
abhitrivedi99 <abhitrivedi99@gmail.com>
2022-07-04 11:18:09 +0530

    feat(authorize-access) _app js updated for access reasons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useEffect, useState } from 'react';
+import { magic } from '../lib/magic-client';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    async function userLogin() {
+      const isLoggedIn = await magic.user.isLoggedIn();
+
+      if (isLoggedIn) {
+        setIsLoading(false);
+        router.push('/');
+      } else {
+        setIsLoading(false);
+        router.push('/login');
+      }
+    }
+    userLogin();
+  });
+  return isLoading ? <div>Loading...</div> : <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
